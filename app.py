c53a0307d038f9a286132f1a0ab56ccb92b5e8d3
PARFAIT-PADO <padreparf@gmail.com>
2024-09-21 12:53:23 +0200

    App commit

---
+++ app.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 17 16:56:25 2023
+
+@author: PERFECTO
+"""
+
+# -*- coding: utf-8 -*-
+import streamlit as st
+
+st.title("Hello, Streamlit!")
+st.write("This is a test app to check deployment issues.")
+
+    
+   
+    
+   
+    
+   
+    
+   
+   
+   
